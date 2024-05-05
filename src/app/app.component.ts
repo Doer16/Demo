ce970e3dc37aa93b68aa48fa237336df1e3837d5
Doer16 <tejk16@gmail.com>
2024-05-04 20:30:14 -0500

    loop thru Object data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  mobiles = ['redmi', 'realme', 'appo', 'vivo', 'samsung'];
+  mobiles = [
+    {
+      "id": 2,
+      "name": "One Plus Nord",
+      "price": 25000,
+      "ram": "8gb",
+      "storage":"128gb"
+    },
+    {
+      "id": 3,
+      "name": "Samsung Galaxy M55",
+      "price": 26500,
+      "ram": "6gb",
+      "storage":"128gb"
+    },
+    {
+      "id": 5,
+      "name": "real me 5",
+      "price": 20000,
+      "ram": "8gb",
+      "storage":"128gb"
+    }
+  ];
 }
