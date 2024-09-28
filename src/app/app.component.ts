832b429e3be23ac9167220ea839a965495260ed0
Doer16 <tejk16@gmail.com>
2024-09-27 22:49:05 -0500

    event binding 11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,27 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  mobiles = [
-    {
-      "id": 2,
-      "name": "One Plus Nord",
-      "price": 25000,
-      "ram": "8gb",
-      "storage":"128gb"
-    },
-    {
-      "id": 3,
-      "name": "Samsung Galaxy M55",
-      "price": 26500,
-      "ram": "6gb",
-      "storage":"128gb"
-    },
-    {
-      "id": 5,
-      "name": "real me 5",
-      "price": 20000,
-      "ram": "8gb",
-      "storage":"128gb"
-    }
-  ];
+  counter: number = 0;
+  name = "xyz";
+
+  increment() {
+    return this.counter += 1;
+  }
+  decrement() {
+    return this.counter -= 1;
+  }
+
+  changeName(e: any) {
+    this.name = e.target.value;
+  }
+
 }
