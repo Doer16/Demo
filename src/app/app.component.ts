c7e99d996839ff7dd776417c9340a5fd9ecea8ab
Doer16 <tejk16@gmail.com>
2024-10-16 15:10:33 -0500

    ngFor 15 video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  data = ["Hyderabad", "Bangalore", "Delhi"];
+  nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  randomNums = ['s', 'w', 'm', 's', 'b'];
+  mobiles = ['redmi', 'realme', 'appo', 'vivo', 'samsung'];
+  mobilesNew = [
+    {
+      "id": 2,
+      "name": "One Plus Nord",
+      "price": 25000,
+      "ram": "8gb",
+      "storage": "128gb"
+    },
+    {
+      "id": 3,
+      "name": "Samsung Galaxy M55",
+      "price": 26500,
+      "ram": "6gb",
+      "storage": "128gb"
+    },
+    {
+      "id": 5,
+      "name": "real me 5",
+      "price": 20000,
+      "ram": "8gb",
+      "storage": "128gb"
+    }
+  ];
 }
