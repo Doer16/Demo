3aca0210ce7342bff9a664c1e0e2eb46fbf9d29b
Doer16 <tejk16@gmail.com>
2024-12-22 20:59:35 -0600

    StringInterpolation,Bindings-Vid-7,8,9,10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  d: string = new Date().toLocaleTimeString();
 
+  timeid = setInterval(() => {
+    this.d = new Date().toLocaleTimeString();
+  }, 1000)
 
-}
+  isDisabled: boolean = false;
+  isHidden: boolean = false;
+
+  isActive: boolean = false;
+
+  applyC1: boolean = true;
+
+  cvar: string = 'blue';
+
+  mystyle: object = {
+    color: 'yellow',
+    background: 'grey',
+    border: '5px solid blue'
+  };
+  hasError: boolean = true;
+} 
